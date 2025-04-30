7c5122a39d102e12673d5d3e8ed2baf44aa30c3d
u634434 <gatibalditiranti@teco.com.ar>
2025-04-30 18:01:31 -0300

    feat: Agregada de path and query

--- Backend/FastAPI/clients.py
+++ Backend/FastAPI/clients.py
@@ -6,15 +6,16 @@ app = FastAPI()
 
 # Entidad client
 class Client(BaseModel):
+    id: int
     name: str
     lastname: str
     age: int
     location: str
 
-clients_list = [Client(name="Gianni", lastname="Tibaldi", age=35, location="General Deheza"),
-                Client(name="Joaquín", lastname="Fernandez", age=65, location="Rio Cuarto"),
-                Client(name="Fernando", lastname="Gomez", age=44, location="Baigorria"),
-                Client(name="Roberto", lastname="Ramirez", age=39, location="Alcira Gigena")]
+clients_list = [Client(id=1,name="Gianni", lastname="Tibaldi", age=35, location="General Deheza"),
+                Client(id=2,name="Joaquín", lastname="Fernandez", age=65, location="Rio Cuarto"),
+                Client(id=3,name="Fernando", lastname="Gomez", age=44, location="Baigorria"),
+                Client(id=4,name="Roberto", lastname="Ramirez", age=39, location="Alcira Gigena")]
 
 @app.get("/clientsjson") 
 async def clientsjson():
@@ -23,6 +24,20 @@ async def clientsjson():
             {"name": "Fernando", "lastname": "Gomez", "age": "44","location": "Baigorria"},
             {"name": "Roberto", "lastname": "Ramirez", "age": "39","location": "Alcira Gigena"}]
 
-@app.get("/clients")
-async def clients():
-    return clients_list
+#Path
+@app.get("/client/{id}")
+async def client(id: int):
+    return search_client(id)
+
+#Query
+@app.get("/clientquery/")
+async def client(id: int):
+    return search_client(id)
+    
+
+def search_client(id: int):
+    clients = filter(lambda client: client.id == id, clients_list)
+    try:
+        return list(clients)[0]
+    except:
+        return {"error": "Client not found"}
