d5ce304db0d807744b91d53e8c204a33f5439f33
u634434 <gatibalditiranti@teco.com.ar>
2025-05-05 18:06:44 -0300

    feat: Se agrego metodo post

--- Backend/FastAPI/clients.py
+++ Backend/FastAPI/clients.py
@@ -12,10 +12,10 @@ class Client(BaseModel):
     age: int
     location: str
 
-clients_list = [Client(id=1,name="Gianni", lastname="Tibaldi", age=35, location="General Deheza"),
-                Client(id=2,name="Joaquín", lastname="Fernandez", age=65, location="Rio Cuarto"),
-                Client(id=3,name="Fernando", lastname="Gomez", age=44, location="Baigorria"),
-                Client(id=4,name="Roberto", lastname="Ramirez", age=39, location="Alcira Gigena")]
+clients_list = [Client(id=1, name="Gianni", lastname="Tibaldi", age=35, location="General Deheza"),
+                Client(id=2, name="Joaquín", lastname="Fernandez", age=65, location="Rio Cuarto"),
+                Client(id=3, name="Fernando", lastname="Gomez", age=44, location="Baigorria"),
+                Client(id=4, name="Roberto", lastname="Ramirez", age=39, location="Alcira Gigena")]
 
 @app.get("/clientsjson") 
 async def clientsjson():
@@ -30,10 +30,21 @@ async def client(id: int):
     return search_client(id)
 
 #Query
-@app.get("/clientquery/")
+@app.get("/client/")
 async def client(id: int):
     return search_client(id)
     
+
+#Operacion POST
+@app.post("/client/")
+async def create_client(client: Client):
+    if search_client_silent(client.id):
+        raise HTTPException(status_code=400, detail="Ya existe un cliente con ese ID")
+    clients_list.append(client)
+    return client
+
+
+
 
 def search_client(id: int):
     clients = filter(lambda client: client.id == id, clients_list)
@@ -41,3 +52,8 @@ def search_client(id: int):
         return list(clients)[0]
     except:
         raise HTTPException(status_code=404, detail="No hay un cliente actualmente")
+    
+
+def search_client_silent(id: int):
+    clients = list(filter(lambda client: client.id == id, clients_list))
+    return clients[0] if clients else None
